1030a41fe7e83bdcdf70252b1d0fc3b3ac683965
Ataeraxia <you@example.com>
2018-04-24 10:36:07 -0400

    Start thenewboston tutorial, so take out code to match his code

--- index.py
+++ index.py
@@ -1,25 +1,10 @@
 import pygame
 
-def main():
+pygame.init()
 
-  pygame.init()
-  logo = pygame.image.load("logo32x32.png")
-  pygame.display.set_icon(logo)
-  pygame.display.set_caption("minimal program")
+screen = pygame.display.set_mode((400, 350))
 
-  screen = pygame.display.set_mode((240, 180))
+pygame.display.update()
 
-  running = True
-
-  # screen.fill((200,50,50))
-  screen.blit(logo, (50, 50))
-  pygame.display.flip()
-
-  while running:
-    for event in pygame.event.get():
-      if event.type == pygame.QUIT:
-        running = False
-    
-
-if __name__=="__main__":
-  main()
+pygame.quit()
+quit()
