82e17b1957a3412eef315fc7efd4d8178a26c86e
Ataeraxia <you@example.com>
2018-04-24 10:52:34 -0400

    Add while loop and quit event

--- index.py
+++ index.py
@@ -3,8 +3,16 @@ import pygame
 pygame.init()
 
 screen = pygame.display.set_mode((400, 350))
+pygame.display.set_caption("Little Guy")
 
 pygame.display.update()
+
+gameExit = False
+
+while not gameExit:
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      gameExit = True
 
 pygame.quit()
 quit()
