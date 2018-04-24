d8866d3fb8c6458122559aaf32848159e0f32b79
Ataeraxia <you@example.com>
2018-04-24 11:50:02 -0400

    Add goals.md and make a first image for sniffer and hopper

---
+++ goals.md
@@ -0,0 +1,9 @@
+When your pet ages enough, it turns into a type of pet with a unique trait
+
+Sniffer has a long nose, and it lets you execute commands from within the game
+
+* Also, sniffer is known to be very loyal and intelligent
+
+Hopper has big silly feet, and it unlocks a new game called Hopper's Cards (basically solitaire)
+
+* Also, hopper is known to be silly and cute
--- index.py
+++ index.py
@@ -1,11 +1,14 @@
+# cSpell:ignore pygame, rect
 import pygame
 
 pygame.init()
 
+white = (240, 240, 240)
+black = (40,20,20)
+red = (200, 40, 40)
+
 screen = pygame.display.set_mode((400, 350))
 pygame.display.set_caption("Little Guy")
-
-pygame.display.update()
 
 gameExit = False
 
@@ -13,6 +16,10 @@ while not gameExit:
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
       gameExit = True
+
+  screen.fill(black, rect=[175,150,50,50])
+  # pygame.draw.rect(screen, red, [175,150,50,50])  # x,y,w,h
+  pygame.display.update()
 
 pygame.quit()
 quit()
